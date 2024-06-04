7b07ed95420591c1a050fc9b96661f11bf3c0ad7
Priyesh Ranjan <pr8pf@umsystem.edu>
2024-06-04 12:35:56 -0500

    Update

--- tasks/mnist.py
+++ tasks/mnist.py
@@ -8,7 +8,8 @@ import torch.nn.functional as F
 from torchvision import datasets, transforms
 
 from dataloader import *
-
+from torch.distributions.normal import Normal
+from torch.distributions.kl import kl_divergence
 
 class Net(nn.Module):
     '''
@@ -19,7 +20,7 @@ class Net(nn.Module):
 
     '''
 
-    def __init__(self):
+    def __init__(self, weight_scale=0.1, rho_offset=-3, zeta=10):
         super(Net, self).__init__()
         # 1 input image channel, 6 output channels, 3x3 square convolution
         # kernel
@@ -29,7 +30,22 @@ class Net(nn.Module):
         self.fc1 = nn.Linear(16 * 6 * 6, 120)  # 6*6 from image dimension
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
-
+        
+        # The BNN part
+        self.layer_param_shapes = self.get_layer_param_shapes()
+        self.mus = nn.ParameterList()
+        self.rhos = nn.ParameterList()
+        self.weight_scale = weight_scale
+        self.rho_offset = rho_offset
+        self.zeta = torch.tensor(zeta, device=self.device)
+        self.sigmas = torch.tensor([1.]*len(self.layer_param_shapes, device=self.device))
+        
+        for shape in self.layer_param_shapes :
+            mu = nn.Parameter(torch.normal(mean=torch.zeros(shape), std=self.weight_scale*torch.ones(shape)))
+            rho = nn.Parameter(self.rho_offset + torch.zeros(shape))
+            self.mus.append(mu)
+            self.rhos.append(rho)
+            
     def forward(self, x):
         # Max pooling over a (2, 2) window
         x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
@@ -48,10 +64,49 @@ class Net(nn.Module):
             num_features *= s
         return num_features
 
-    def total_params(self) :
-        for layer in model :
-            total += (layer[]+1) * layer[]
-        return total
+    def get_layer_param_shapes(self):
+        layer_param_shapes = []
+        for i in range(self.num_layers + 1):
+            if i == 0:
+                W_shape = (self.input_dim, self.hidden_dim)
+                b_shape = (self.hidden_dim,)
+            elif i == self.num_layers:
+                W_shape = (self.hidden_dim, self.output_dim)
+                b_shape = (self.output_dim,)
+            else:
+                W_shape = (self.hidden_dim, self.hidden_dim)
+                b_shape = (self.hidden_dim,)
+            layer_param_shapes.extend([W_shape, b_shape])
+        return layer_param_shapes
+
+    def transform_rhos(self, rhos) :
+        return [F.softplus(rho) for rho in rhos]
+    
+    def transform_gaussian_samples(self, mus, rhos, epsilons) :
+        self.sigmas = self.transform_rhos(rhos)
+        samples = []
+        for j in range(len(mus)):
+            samples.append(mus[j] + self.sigmas[j]*epsilons[j])
+        return samples
+
+    def samples_epsilons(self, param_shapes) :
+        epsilons = [torch.normal(mean=torch.zeros(shape),
+                                 std =0.001*torch.ones(shape)).to(self.device) for shape in param_shapes]
+        return epsilons
+    
+    def log_softmax_likelihood(self, yhat_linear, y) :
+        return torch.nansum(y * F.log_softmax(yhat_linear), dim=0)
+    
+    def combined_loss_personal(self, output, label_one_hot, params, mus, sigmas, mus_local, sigmas_local, num_batches) :
+        log_likelihood_sum = torch.sum(self.log_softmax_likelihood(output, label_one_hot))
+        KL_q_w = sum([torch.sum(kl_divergence(Normal(mus[i], sigmas[i]), Normal(mus_local[i].detach(), sigmas_local[i].detach()))) for i in range(len(params))])
+        
+        return 1.0 / num_batches * (self.zeta * KL_q_w) - log_likelihood_sum
+    
+    def combined_loss_local(self, params, mus, sigmas, mus_local, sigmas_local, num_batches):
+        KL_q_w = sum([torch.sum(kl_divergence(Normal(mus[i].detach(), sigmas[i].detach()),
+                                              Normal(mus_local[i], sigmas_local[i]))) for i in range(len(params))])
+        return 1.0 / num_batches * (self.zeta*KL_q_w)
 
 def getDataset():
     dataset = datasets.MNIST('./data',
